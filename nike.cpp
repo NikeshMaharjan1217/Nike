282248301172dc4c7c614fa19638323b99adc658
NikeshMaharjan1217 <NikeshMaharjan1217@gmail.com>
2019-07-23 11:47:29 +0545

    value Added

--- nike.cpp
+++ nike.cpp
@@ -2,23 +2,7 @@
 #include<conio.h>
 int main()
 {
-	int i,x,n=6,flag=0;
-	int a[]={5,15,28,33,22,1};
-	printf("Enter the value:");
-	scanf("%d",&x);
-	for(i=0;i<n;i++)
-	{
-		if(a[i]==x)
-		{
-		printf("%d is found in %d",x,i+1);
-		flag=1;
-		break;
-		} 
-	
-    }
-	/*if(flag==0)
-	{
-	printf("%d is not in array",x);
-    }
-	getch();*/
+	int a=5,b=6;
+	int c=a+b;
+	printf("Add=%d",c);
 }
